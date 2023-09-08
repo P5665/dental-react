60cfeb2fb8cb03abd5973e0d1a0aae60fb836f6e
Ben Peterson <benp5665@gmail.com>
2023-09-08 10:27:57 -0700

    fixed service cards and pricing sectional

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -10,7 +10,7 @@ const Pricing = () => {
 				current='Pricing'
 				image={subHeaderImage}
 			/>
-			<Container>
+			<Container className='pt-3'>
 				<Row>
 					<Col className='mt-3'>
 						<p>At our dental practice, we believe that everyone deserves access to high-quality dental care. Our transparent and competitive pricing ensures that you receive exceptional dental services without hidden fees or surprises. Explore our pricing options below:</p>
@@ -25,24 +25,31 @@ const Pricing = () => {
 					<Col></Col>
 				</Row>
 			</Container>
-			<GenericServicesList />
+			<div className='pb-5'>
+				<GenericServicesList />
+			</div>
+			<div className='bg-light section'>
+				<Container>
+					<Row>
+						<Col
+							xs='10 offset-1'
+							md='8 offset-md-2'
+						>
+							<h2 className='section-heading text-dark text-center'>Payment Options</h2>
+							<div className='line mb-5 mx-auto'></div>
+							<p>We offer flexible payment options to accommodate your budget:</p>
+							<ul>
+								<li>Insurance: We accept most dental insurance plans and will gladly assist with claims.</li>
+								<li>Financing: Explore our financing options to spread the cost of treatment over time.</li>
+								<li>Credit Cards: We accept major credit cards for your convenience.</li>
+								<li>Cash: We welcome cash payments.</li>
+							</ul>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 			<Container className='section pt-3'>
 				<Row>
-					<Col
-						xs='10 offset-1'
-						md='8 offset-md-2'
-						className='mt-5'
-					>
-						<h2 className='section-heading text-dark text-center'>Payment Options</h2>
-						<div className='line mb-5 mx-auto'></div>
-						<p>We offer flexible payment options to accommodate your budget:</p>
-						<ul>
-							<li>Insurance: We accept most dental insurance plans and will gladly assist with claims.</li>
-							<li>Financing: Explore our financing options to spread the cost of treatment over time.</li>
-							<li>Credit Cards: We accept major credit cards for your convenience.</li>
-							<li>Cash: We welcome cash payments.</li>
-						</ul>
-					</Col>
 					<Col
 						xs='10 offset-1'
 						md='8 offset-md-2'
